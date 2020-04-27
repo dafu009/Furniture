16752313110e7ff3321446cba1b68ddea3b979ec
大幅 <527431542@qq.com>
2020-04-27 20:35:03 +0800

    update

--- WebRoot/js/register.js
+++ WebRoot/js/register.js
@@ -1,3 +1,9 @@
+$(function(){
+	$("#city").click(function (e) {
+		SelCity(this,e);
+	});
+})
+
 window.onload = () => {
   const vm = new Vue({
     el: '#app',
@@ -14,6 +20,7 @@ window.onload = () => {
       goHomePage () {
         window.location.href = "index.html"
       },
+
       resister () {
         if (!this.username) {
           sweetAlert('Oops..', '请输入用户名', 'error')
@@ -23,10 +30,24 @@ window.onload = () => {
           sweetAlert('Oops..', '请输入完整信息', 'error')
           return
         }
+        //规定手机号码的格式        
+        var myreg=/^[1][3,4,5,7,8][0-9]{9}$/;
+        if (!myreg.test(this.telephone)) {
+        	sweetAlert('Oops..', '请输入正确的手机号码格式', 'error')
+        	return
+        } 
+
         if (!this.password) {
           sweetAlert('Oops..', '请输入密码', 'error')
           return
         }
+      //规定密码不能少于6位且由数字字母组成
+        var mimareg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$/;
+        if (!mimareg.test(this.password)) {
+        	sweetAlert('Oops..', '密码不能少于6位且由数字字母组成', 'error')
+        	return
+        } 
+        
         if (!this.checkpass) {
           sweetAlert('Oops..', '请输入确认密码', 'error')
           return
@@ -54,13 +75,8 @@ window.onload = () => {
             if (code === 200) {
               window.location.href = 'login.html'
             }
-<<<<<<< HEAD
             else if (code === 100) {
             	sweetAlert('Oops..', '用户名已存在，请重新输入', 'error')
-=======
-            if (code === 100) {
-                sweetAlert('Oops..', '用户名已存在', 'error')
->>>>>>> master
               }
           })
           .catch((err) => {
--- src/FurnitureC/controller/OrderController.java
+++ src/FurnitureC/controller/OrderController.java
@@ -45,7 +45,7 @@ public class OrderController {
 	
 	
 	/**
-	 * ÊµÏÖ/addorderµÄ¶©µ¥Ôö¼Ó
+	 * Êµï¿½ï¿½/addorderï¿½Ä¶ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 	 */
 	@RequestMapping("/addOrder")
 	@ResponseBody
@@ -68,14 +68,14 @@ public class OrderController {
 				double totalMoney = 0;
 				double money1,money2 = 0;
 				String strmoney1,strmoney2;
-				//¹Ì¶¨Êı×Ö×ª»¯¸ñÊ½
+				//ï¿½Ì¶ï¿½ï¿½ï¿½ï¿½ï¿½×ªï¿½ï¿½ï¿½ï¿½Ê½
 				java.text.DecimalFormat format = new java.text.DecimalFormat("#0.00");
 		
 				if(idlist.size() == 1){
 					num1 = Integer.parseInt(idlist.get(0).get("num").toString());
 					inPrice = Double.parseDouble(idlist.get(0).get("inPrice").toString());
 	//				strmoney1 = idlist.get(0).get("inPrice").toString();
-	//				inPrice = Double.parseDouble(strmoney1);//×ª»»³ÉdoubleÀàĞÍ
+	//				inPrice = Double.parseDouble(strmoney1);//×ªï¿½ï¿½ï¿½ï¿½doubleï¿½ï¿½ï¿½ï¿½
 					money1 = Double.parseDouble(format.format(inPrice));
 					totalNum = num1;
 					totalMoney = money1*num1;
@@ -107,11 +107,11 @@ public class OrderController {
 					String num = idlist.get(k).get("num").toString();
 					String price = idlist.get(k).get("inPrice").toString();
 					orderService.AddOrderItem(orderFormID, goodsID, num, price);
-					//´Ó¹ºÎï³µÖĞÒÆ³ı
+					//ï¿½Ó¹ï¿½ï¿½ï³µï¿½ï¿½ï¿½Æ³ï¿½
 					shoppingCartService.DeleteShoppingCartWithOrder(userID, goodsID);
 				}
 				flag = 1;
-				message = "³É¹¦";
+				message = "ï¿½É¹ï¿½";
 				result.put("flag", flag);
 				result.put("message", message);
 				code = 200;
@@ -124,7 +124,7 @@ public class OrderController {
 				return map;	 
 			}else{
 				flag = 0;
-				message = "Ê§°Ü";
+				message = "Ê§ï¿½ï¿½";
 				result.put("flag", flag);
 				result.put("message", message);
 				code = 0;
@@ -140,7 +140,7 @@ public class OrderController {
 		}catch(Exception e){
 			System.out.println(e);
 			flag = 0;
-			message = "Ê§°Ü";
+			message = "Ê§ï¿½ï¿½";
 			result.put("flag", flag);
 			result.put("message", message);
 			code = 0;
@@ -158,7 +158,7 @@ public class OrderController {
 	
 	
 	/**
-	 * ÊµÏÖ/orderÎÒµÄ¶©µ¥ÏÔÊ¾
+	 * Êµï¿½ï¿½/orderï¿½ÒµÄ¶ï¿½ï¿½ï¿½ï¿½ï¿½Ê¾
 	 */
 	@RequestMapping("/viewOrder")
 	@ResponseBody
@@ -199,7 +199,7 @@ public class OrderController {
 				result.put("orderlist", ALLorder);
 				code = 200;
 				state = "success";
-				message = "³É¹¦";
+				message = "æˆåŠŸ";
 				map.put("code", code);
 				map.put("state", state);
 				map.put("message", message);
@@ -211,7 +211,7 @@ public class OrderController {
 				result.put("orderlist", ALLorder);
 				code = 0;
 				state = "fail";
-				message = "Ê§°Ü";
+				message = "Ê§ï¿½ï¿½";
 				map.put("code", code);
 				map.put("state", state);
 				map.put("message", message);
@@ -225,7 +225,7 @@ public class OrderController {
 			result.put("orderlist", ALLorder);
 			code = 0;
 			state = "fail";
-			message = "Ê§°Ü";
+			message = "Ê§ï¿½ï¿½";
 			map.put("code", code);
 			map.put("state", state);
 			map.put("message", message);
